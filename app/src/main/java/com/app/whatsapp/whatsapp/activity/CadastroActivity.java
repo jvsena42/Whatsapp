6b9445281f91073022ed27cef17a44441612b960
jvsena42 <jvsena16@gmail.com>
2019-03-21 18:50:17 -0500

    +Cadastro usuario

--- app/src/main/java/com/app/whatsapp/whatsapp/activity/CadastroActivity.java
+++ app/src/main/java/com/app/whatsapp/whatsapp/activity/CadastroActivity.java
@@ -1,15 +1,27 @@
 package com.app.whatsapp.whatsapp.activity;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.app.whatsapp.whatsapp.R;
+import com.app.whatsapp.whatsapp.config.ConfiguracaoFirebase;
+import com.app.whatsapp.whatsapp.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class CadastroActivity extends AppCompatActivity {
 
     private EditText campoNome, campoEmail, campoSenha;
+    private FirebaseAuth autentiacao;
 
 
     @Override
@@ -22,9 +34,49 @@ public class CadastroActivity extends AppCompatActivity {
         campoSenha = findViewById(R.id.editSenha);
     }
 
-    public void cadastrarUsuario(View view){
+    public void cadastrarUsuario(Usuario usuario){
+        autentiacao = ConfiguracaoFirebase.getFirebaseAuth();
+        autentiacao.createUserWithEmailAndPassword(
+            usuario.getEmail(),usuario.getSenha()
+        ).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    finish();
+                }else {
+                    String excessao = "";
+                    try {
+                        throw task.getException();
+                    }catch (FirebaseAuthWeakPasswordException e){
+                        excessao = "Digite uma senha mais forte!";
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        excessao = "Digite um e-mail válido";
+                    }catch (FirebaseAuthUserCollisionException e){
+                        excessao = "Usuário já cadastrado";
+                    }catch (Exception e){
+                        excessao = "Erro ao cadastrar usuário: " + e.getMessage();
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(CadastroActivity.this,excessao,Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
+    public void validarCadastroUsuario(View view){
         String textoNome = campoNome.getText().toString();
         String textoEmail = campoEmail.getText().toString();
         String textoSenha = campoSenha.getText().toString();
+
+        if (!textoNome.isEmpty() || textoEmail.isEmpty() || textoSenha.isEmpty()){
+            Usuario usuario = new Usuario();
+            usuario.setNome(textoNome);
+            usuario.setEmail(textoEmail);
+            usuario.setSenha(textoSenha);
+            cadastrarUsuario(usuario);
+        }else {
+            Toast.makeText(CadastroActivity.this,"Preencha todos os campos!",Toast.LENGTH_LONG).show();
+        }
     }
 }
