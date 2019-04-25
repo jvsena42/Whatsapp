d6853ee38bcac7bdde0dc93974eb53262fc393a7
jvsena42 <jvsena16@gmail.com>
2019-04-25 10:27:14 -0500

    +Configurar escolha de foto

--- app/src/main/java/com/app/whatsapp/whatsapp/activity/ConfiguracoesActivity.java
+++ app/src/main/java/com/app/whatsapp/whatsapp/activity/ConfiguracoesActivity.java
@@ -2,12 +2,16 @@ package com.app.whatsapp.whatsapp.activity;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.app.whatsapp.whatsapp.R;
 import com.app.whatsapp.whatsapp.helper.Permissao;
@@ -19,6 +23,10 @@ public class ConfiguracoesActivity extends AppCompatActivity {
             Manifest.permission.CAMERA
     };
 
+    private ImageButton imageButtonCamera, imageButtonGaleria;
+    private static final int SELECAO_CAMERA = 100;
+    private static final int SELECAO_GALERIA = 200;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,11 +35,33 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         //Validar permissoes
         Permissao.validarPermissoes(permissoesNecessarias,this,1);
 
+        imageButtonCamera = findViewById(R.id.imageButtonCamera);
+        imageButtonGaleria = findViewById(R.id.imageButtongaleria);
+
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Configuracoes");
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        imageButtonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(intent,SELECAO_CAMERA);
+                }
+
+            }
+        });
+
+        imageButtonGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
